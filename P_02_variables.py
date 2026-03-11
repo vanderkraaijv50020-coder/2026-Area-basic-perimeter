3da0bb0fae004c5d9afe22d480222e00eb2fcdf0
vanderkraaijv50020-coder <vanderkraaijv50020@masseyhigh.school.nz>
2026-03-12 10:14:49 +1300

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,15 @@
+# Code to learn variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+print(greeting)
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print( num_string * 5)
+
+# Constants are uppercase
+
+a_01_my_num = 7
